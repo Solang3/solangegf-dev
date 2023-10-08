663c33aa1eed887afde9c089e7110e622d8e83d9
Solange Gonzalez Ferrero <solangegf@gmail.com>
2023-10-08 11:05:58 -0300

    Toggle menu for mobile

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, HostListener } from '@angular/core';
+import { MobileMenuComponent } from '../mobile-menu/mobile-menu.component';
+
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.less']
 })
+
 export class NavComponent {
+  @ViewChild('mobileMenu') mobileMenu!: MobileMenuComponent;
+
+  // Close the menu when the window width changes
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    if (window.innerWidth >= 768) {
+      this.mobileMenu.isMenuOpen = false;
+    }
+  }
 
 }
